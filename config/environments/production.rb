4191f54f19d66f28f8fc76e6483e49553af95541
yash-kreeti <yash.barman@kreeti.com>
2023-09-28 13:29:32 +0530

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,18 +92,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'https' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'blooming-shore-38050-fa03fc163527.herokuapp.com', protocol: 'https' }
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  # config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   user_name:
-  #   password:
-  #   authentication: 'plain',
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.smtp[:user_name],
+    password: Rails.application.credentials.smtp[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
